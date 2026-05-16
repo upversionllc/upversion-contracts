0bba77349f143e2c03e8b8438eb45e8809da4026
upversionllc <mahezabin@upversion.net>
2026-05-16 14:24:36 -0400

    Update sam.js

--- api/sam.js
+++ api/sam.js
@@ -1,35 +1,26 @@
-export default async function handler(req, res) {
+module.exports = async function(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
   
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-
-  const SAM_KEY = process.env.SAM_API_KEY;
-  const naicsCodes = ["541613", "611420", "541511", "424120"];
-  const allResults = [];
-
-  const today = new Date();
-  const past = new Date();
+  var SAM_KEY = process.env.SAM_API_KEY;
+  var results = [];
+  var codes = ["541613", "611420", "541511", "424120"];
+  
+  var today = new Date();
+  var past = new Date();
   past.setDate(past.getDate() - 90);
-  const fmt = d => ${String(d.getMonth()+1).padStart(2,"0")}/${String(d.getDate()).padStart(2,"0")}/${d.getFullYear()};
-
-  for (const naics of naicsCodes) {
-    try {
-      const url = https://api.sam.gov/opportunities/v2/search?api_key=${SAM_KEY}&naicsCode=${naics}&limit=10&postedFrom=${fmt(past)}&postedTo=${fmt(today)}&active=true;
-      const r = await fetch(url);
-      const data = await r.json();
-      if (data.opportunitiesData) allResults.push(...data.opportunitiesData);
-    } catch(e) { console.error(naics, e.message); }
+  
+  function fmt(d) {
+    return (d.getMonth()+1).toString().padStart(2,"0") + "/" + d.getDate().toString().padStart(2,"0") + "/" + d.getFullYear();
   }
-
-  const seen = new Set();
-  const deduped = allResults.filter(o => {
-    const id = o.noticeId || o.solicitationNumber;
-    if (seen.has(id)) return false;
-    seen.add(id); return true;
-  });
-
-  res.status(200).json({ opportunities: deduped, total: deduped.length });
+  
+  for (var i = 0; i < codes.length; i++) {
+    try {
+      var url = "https://api.sam.gov/opportunities/v2/search?api_key=" + SAM_KEY + "&naicsCode=" + codes[i] + "&limit=10&postedFrom=" + fmt(past) + "&postedTo=" + fmt(today) + "&active=true";
+      var r = await fetch(url);
+      var data = await r.json();
+      if (data.opportunitiesData) results = results.concat(data.opportunitiesData);
+    } catch(e) {}
+  }
+  
+  res.status(200).json({ opportunities: results, total: results.length });
 }
