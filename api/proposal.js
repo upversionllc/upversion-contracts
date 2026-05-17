a7ad542d357c6923793145ce71d110ae6c1cc191
upversionllc <mahezabin@upversion.net>
2026-05-17 09:42:24 -0400

    Update proposal.js

--- api/proposal.js
+++ api/proposal.js
@@ -24,6 +24,14 @@ export default async function handler(req, res) {
     });
 
     const data = await response.json();
+
+    if (data.error) {
+      return res.status(200).json({ 
+        debugError: JSON.stringify(data.error),
+        fullResponse: JSON.stringify(data)
+      });
+    }
+
     res.status(200).json(data);
   } catch(e) {
     res.status(500).json({ error: e.message });
